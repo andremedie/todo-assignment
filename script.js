b0a8a26ec7faff258835b3a06fad9fadf168db08
andremedie <32762205+andremedie@users.noreply.github.com>
2017-11-24 10:04:21 +0100

    todo7

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-var stuffToDo = [
-    "Klipp gräset", 
-    "Betala räkningar",
-    "Köp mjölk", 
-    "Spika upp tavlor"
-    ];
+$(document).ready(function(){
+
+        var user = "user";
+        var password = "password";
+
+        $(".logout").hide();
+        $(".div2").hide();
+        $(".div3").hide();
+        $(".login").show();
+        $(".main").show();
+        // -a-a-a
+    
+        if (sessionStorage.doList == null) {
+
+            var stuffToDo = [
+                "Klipp gräset", 
+                "Betala räkningar",
+                "Köp mjölk", 
+                "Spika upp tavlor"
+                ];
+
+                var json_str = JSON.stringify(stuffToDo);
+                sessionStorage.doList = json_str;
+
+        };
+    
+        $(".loginButton").click(function(){
+            
+              if ( $(".user").val() == user && $(".password").val() == password ) {
+                
+                    show.div3
+                    
+              console.log("rättlösenord")
+                } else {
+                    
+                    show.div2
+                    console.log("fellösenord")
+              } 
+
+        });
+    
+    });
